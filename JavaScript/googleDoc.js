19c6ac62935290ffdfada437890e407e0f6af9b2
Graeme Broomhall <graeme.broomhall@my.jcu.edu.au>
2016-05-26 13:45:30 +1000

    added alert box with contact form, fixed gallery

--- JavaScript/googleDoc.js
+++ JavaScript/googleDoc.js
@@ -41,6 +41,7 @@ jQuery( document ).ready(function( $ ) {
                 $inputs.prop("disabled", false);
             });
             event.preventDefault();
+            alert("Contact form sent!");
         } else {
             return false;
         }
